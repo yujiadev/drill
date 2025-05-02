38a1229b31101349cccecc8f1b88eed0e01138e3
Yu Jia <yujiadev@outlook.com>
2025-05-02 16:45:54 +0800

    update https.go

--- pkg/proxy/https.go
+++ pkg/proxy/https.go
@@ -3,7 +3,9 @@ package proxy
 import (
 	"log"
 	"fmt"
+	"bufio"
 	"net"
+	"net/http"
 )
 
 type HttpsProxy struct {
@@ -37,5 +39,18 @@ func (pxy *HttpsProxy) Run() {
 }
 
 func handleConnectRequest(conn net.Conn) {
-	fmt.Println("Handle HTTP CONNECT Request")	
+	request, err := http.ReadRequest(bufio.NewReader(conn))
+
+	if err != nil {
+		log.Fatalf("handleConnectRequest read HTTP request error. %s", err)
+	}
+
+	// Only support HTTP CONNECT method 
+	if request.Method != "CONNECT" {
+		log.Fatalf("handleConnectRequest() error. Not supported HTTP method")
+	}
+
+	fmt.Printf("%s\n", request.Host)
+
+	conn.Close()
 }
