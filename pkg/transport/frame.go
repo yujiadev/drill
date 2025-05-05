bdd625845b9155b3403d0c5178190d5d94241af3
Yu Jia <yujiadev@outlook.com>
2025-05-05 23:32:39 +0800

    Refactoring Packet

--- pkg/transport/frame.go
+++ pkg/transport/frame.go
@@ -26,10 +26,28 @@ type Frame struct {
 	Raw []byte
 }
 
-func NewFrame(method byte, seq, src, dst uint64, payload []byte) {
+func NewFrame(method byte, seq, src, dst uint64, payload []byte) Frame {
+	time := uint64(time.Now().Unix())
+
 	raw := []byte{ method }
+	raw, _ = binary.Append(raw, binary.BigEndian, time)
+	raw, _ = binary.Append(raw, binary.BigEndian, seq)
+	raw, _ = binary.Append(raw, binary.BigEndian, src)
+	raw, _ = binary.Append(raw, binary.BigEndian, dst)
 
+	payloadSize := uint32(len(payload))
+	raw, _ = binary.Append(raw, binary.BigEndian, payloadSize)
+	raw = append(raw, payload...)
 
+	return Frame {
+		method,
+		int64(time),
+		seq,
+		src,
+		dst,
+		payload,
+		raw,
+	}
 }
 
 func ParseFrame(data *[]byte) (Frame, error) {
